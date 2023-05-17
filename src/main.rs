927b34b80bb90090cc358e899553cb3916f09dca
Dylan <carringtonfair.dylan@gmail.com>
2023-05-17 00:58:03 -0400

    making notes on what to code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
+   // Order of program:
+   // 1. Retrieve number from user
+   // 2. Find all primes under that number
+   // - If it is even, and not 2, then not a prime
+   // - Maybe use the various prime checks (Mersenne and Fermat)
+   // 3. If not, see if two primes add to it
+   // - Find all primes less than number input and see if adding two of them equals that number
+   //
    // A prime number is a natural number greater than 1 that is not
    // a product of two smaller natural numbers.
 
